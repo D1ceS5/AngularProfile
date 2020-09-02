a1563107ab831005834191a45859193d35b02d87
D1ceS5 <makdggf@gmail.com>
2020-09-03 01:13:59 +0300

    qweqwewqewq

---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+	selector: 'my-app',
+	template: `<div class="page-header">
+					<h1 style="margin-left:35%" >Maxxx <small>The Angular Profile Project</small></h1>
+				</div>
+				<div>
+					<div class="media" style="margin-left:25%" >
+					<a class="pull-left" href="#" >
+						<img class="media-object" width="150px" height="150px" src="https://startupsac.com/wp-content/uploads/2015/10/AngularJS-Logo.png" />
+					</a>
+						<div  class="media-body">
+							<h4 class="media-heading" > Angular </h4>
+							Angular is a TypeScript-based open-source web <br>
+							application framework led by the Angular Team<br>
+							at Google and by a community of individuals and<br>
+						    corporations. Angular is a complete rewrite from the<br>
+							same team that built AngularJS.
+						</div>
+					</div>
+					<div class="media" style="margin-left:25%" >
+					<a class="pull-left" href="#" >
+						<img class="media-object" width="150px" height="150px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png" />
+					</a>
+						<div  class="media-body">
+							<h4 class="media-heading" > JavaScript </h4>
+							JavaScript, often abbreviated as JS, is a<br>
+							 programming language that conforms to the <br>
+							 ECMAScript specification. JavaScript is high-level,<br>
+							  often just-in-time compiled, and multi-paradigm.<br>
+							   It has curly-bracket syntax, dynamic typing,<br>
+							    prototype-based object-orientation, and first-class functions.
+						</div>
+					</div>
+					<div class="media" style="margin-left:25%" >
+					<a class="pull-left" href="#" >
+						<img class="media-object" width="150px" height="150px" src="https://files.virgool.io/upload/users/44716/posts/ig4a3nyuqqu3/rtux15pcpgs3.png" />
+					</a>
+						<div  class="media-body">
+							<h4 class="media-heading" > NodeJS </h4>
+							Node.js is an open-source, cross-platform, JavaScript<br>
+							 runtime environment that executes JavaScript code<br>
+							  outside a web browser.
+						</div>
+					</div>
+					<hr>
+					<h1 style="margin-left:35%" >Sertificates <small>(of course fake)</small></h1>
+					<div style="margin-left:22%" class="thumbnails">
+  						<img class="card-img-top" src="https://i.pinimg.com/originals/e4/73/eb/e473eb024b1bbf990b888b7fd3c79aa2.jpg" alt="Card image cap">
+  						<div class="card-body">
+						  <h3 style="margin-left:20%" >Hogwarts<small>(Griffindor)</small></h3>
+  						</div>
+					</div>
+					<hr>
+					<h1 style="margin-left:45%" >Meeeee</h1>
+					<div style="margin-left:38%" class="thumbnails">
+  						<img class="card-imgtop" src="https://pbs.twimg.com/profile_images/567077338257842176/WNXWoUZc_400x400.jpeg" alt="Card image cap">
+  						
+					</div>
+					<hr>
+					<a style="margin-left:20%" href="https://github.com/D1ceS5"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Octicons-logo-github.svg/1200px-Octicons-logo-github.svg.png" /></a>
+				</div>
+				`
+				
+})
+export class AppComponent{
+	
+}
